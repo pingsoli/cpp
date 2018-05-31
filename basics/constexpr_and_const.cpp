a1fc7a8f3d80be14fc3b7df0549b52b78d164a1f
16.04.3 <pingsoli@163.com>
2018-05-31 13:36:53 +0800

    update something

--- basics/constexpr_and_const.cpp
+++ basics/constexpr_and_const.cpp
@@ -3,7 +3,10 @@
 // `constexpr` is c++11 standardation.
 //
 // The advantage of constexpr ?
-// make sematics more abvious
+// It's explicit and strong.
+//
+// What does 'constexpr' mean ?
+// constexpr variable is guaranteed to have a value available at compile time.
 //
 // Summary:
 // using constexpr when function returns a constexpr.
@@ -19,6 +22,8 @@ constexpr int sqr1(int arg)
 int sqr2(int arg)
 { return arg * arg; }
 
+////////////////////////////////////////////////////////////////////////////////
+// 'constexpr variable is guaranteed to have a value available at compile time.
 const     double PI1 = 3.1415926;
 constexpr double PI2 = 3.1415926;
 
@@ -26,23 +31,46 @@ constexpr double PI2 = 3.1415926;
 // constexpr double PI3 = PI2; // ok
 
 ///////////////////////////////////////////////////////////////////////////////
-//
 // A recursive function never returns constexpr
-
-int fibonacci(int n)
-{
-  if (n == 0 || n == 1)
-    return n;
-
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+class Test {
+public:
+  static const int a  = 3;
+};
+
+class Test1 {
+public:
+  static constexpr int a = 3;
+};
 
 int main(int argc, char** argv)
 {
 //  std::cout << "9^2 = " << sqr1(9) << std::endl;
 //  std::cout << fibonacci(10) << std::endl;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // run-time and compile-time variable ?
+  // static const members or const variable could either mean a compile time
+  // value or a runtime value. but constexpr only mean the variable is compile
+  // time.
+
+//  int run_time;
+//  std::cin >> run_time;
+//
+//  static const int compile_time = run_time;
+//  std::cout << compile_time;
+  //////////////////////////////////////////////////////////////////////////////
+
+  std::cout << Test::a << std::endl; // ok, compile time value can have value.
+
+  // error, compile time constants don't have address.
+  std::cout << Test::a << "(" << &Test::a << ")" << std::endl;
 
   return 0;
 }
--- c/min_max_macro.c
+++ c/min_max_macro.c
@@ -4,12 +4,12 @@
  */
 #include <stdio.h>
 
-//#define max(a,b) \
+/#defin max(a,b) \
 //  ({ __typeof__ (a) _a = (a); \
 //     __typeof__ (b) _b = (b); \
 //     _a > _b ? _a : _b; })
 //
-//#define min(a,b) \
+//# min(a,b) \
 //  ({ __typeof__ (a) _a = (a); \
 //     __typeof__ (b) _b = (b); \
 //     _a < _b ? _a : _b; })
